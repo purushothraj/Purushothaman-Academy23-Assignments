efce860fea4a2be226aad0b893a405c10ecfd4be
purushothraj <purushothaman@metamation.com>
2023-01-24 11:34:35 +0530

    A03: Initial commit

---
+++ A03/P01/SpellingBee/SpellingBee/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.ExceptionServices;
+using System.Linq;
+using System.IO.IsolatedStorage;
+
+namespace SpellingBee{
+   internal class Program{
+      static void Main(string[] args){
+         Dictionary<char, List<string>> FullDict = BuildDictionary("D:\\OneDrive - Trumpf Metamation Pvt Ltd\\Desktop\\words\\words.txt");
+         char[] letters = new[] { 'U', 'X', 'A', 'T', 'L', 'N', 'E' };
+         List<string> Validlist = BuildValidList(letters, FullDict);
+         Dictionary<char,List<string>> quicklist= new Dictionary<char,List<string>>();
+         
+
+
+      }
+
+      static Dictionary<char, List<string>> BuildDictionary(string path)
+      {
+         Dictionary<char, List<string>> dict = new Dictionary<char, List<string>>();
+         string[] Words = File.ReadAllLines(path);
+         List<string> words = new List<string>();
+         int totalWords = Words.Length;
+         char Lastchar = 'A';
+         int j = 0,length;
+         for (int i = 0; i < 26; i++){
+            words = new List<string>();
+            maxlength[i] = 0;
+            for(; j < totalWords;){
+               if (Words[j][0].Equals(Lastchar)){
+                  length = Words[j].Length;
+                  if (maxlength[i] < length) maxlength[i]= length;
+                  if (length>3) words.Add(Words[j]);
+                  j++;
+               }else{
+                  dict.Add(Lastchar,words);
+                  Lastchar = Words[j][0];
+                  break;
+               }
+            }
+         }dict.Add(Lastchar, words);
+         return dict;
+      }
+      static List<string> BuildValidList(char[] letters, Dictionary<char, List<string>> FullDict){
+         List<string> Validlist = new List<string>();
+         foreach (char c in letters)
+         {
+            List<string> Templist = new List<string>();
+            foreach (string s in FullDict[c])
+            {
+               if (s.Contains(letters[0])) Templist.Add(s);
+            }
+            bool valid = true;
+            foreach (string s1 in Templist)
+            {
+               for (int i = 0; i < s1.Length; i++)
+               {
+                  if (!letters.Contains(s1[i]))
+                  {
+                     valid = false;
+                     break;
+                  }
+               }
+               if(valid) Validlist.Add(s1);
+            }
+         }
+         return Validlist;
+      }
+      static int[] maxlength = new int[26];
+   }
+}
---
+++ A03/P02/GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessNumber
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         double value = 0, counter = 0;
+         Console.WriteLine("think of a number from 0 to 127\n");
+         Console.WriteLine("Press 'y' for yes any other key to no");
+         for(int i = 1; i < 8; i++)
+         {
+            Console.WriteLine("Is the remainder divided by " + Math.Pow(2,i) + " is greater than " + (Math.Pow(2,i-1)-1) + "?");
+            string temp = Console.ReadLine();
+            if (temp.Equals("y"))
+            {
+               value = value + Math.Pow(2, counter);
+            }
+            counter++;
+         }
+         Console.WriteLine("the number is " + value);
+      }
+   }
+}
